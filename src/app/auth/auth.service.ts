86ac01df1859debaa181326009eaeeb765d26019
John Long <johnlong@amcsgroup.com>
2020-08-07 15:14:51 +0100

    finished signup funtionality

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface AuthResponseData {
   idToken: string;
@@ -24,6 +26,18 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
+      ).pipe(
+        catchError(errorRes => {
+          let errorMessage = 'An unknown error occurred!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email already exists';
+          }
+          return throwError(errorMessage);
+        })
       );
     }
 }
